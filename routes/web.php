579732d9558d062314e8555fe5eabb1e50d82b47
laura-caldaronello <caldaronello.l@gmail.com>
2021-05-12 14:41:52 +0200

    cambiato in solo main controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','HomeController@index')->name('pagina-home');
+Route::get('/','MainController@home')->name('pagina-home');
 
-Route::get('/vestiti','VestitiController@index')->name('pagina-vestiti');
+Route::get('/vestiti','MainController@vestiti')->name('pagina-vestiti');
