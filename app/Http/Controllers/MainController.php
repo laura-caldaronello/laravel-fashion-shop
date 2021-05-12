98050111af4ea615374b81bfe6bf9e851e48ab44
laura-caldaronello <caldaronello.l@gmail.com>
2021-05-12 15:50:51 +0200

    collegato database

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Dress;
 
 class MainController extends Controller
 {
@@ -12,6 +13,11 @@ class MainController extends Controller
     }
 
     public function vestiti() {
-        return view('vestiti');
+        $dresses = Dress::all();
+        $data = [
+            'message' => 'ecco i vestiti',
+            'dresses' => $dresses
+        ];
+        return view('vestiti',$data);
     }
 }
--- resources/views/vestiti.blade.php
+++ resources/views/vestiti.blade.php
@@ -4,4 +4,7 @@
 
 @section('content')
     <h1>Vestiti</h1>
+    <h2>{{$message}}</h2>
+    @dump($dresses)
+    {{-- <h2>{{$dresses}}</h2> --}}
 @endsection
